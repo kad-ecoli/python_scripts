3c017c432ee057589abb227c0499b28568225d85
Chengxin Zhang <zcx@dcmb-yzl09.med.umich.edu>
2017-01-16 21:52:18 -0500

    convert fasta to a list of entry length by fasta2len.py

---
+++ fasta2len.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+docstring='''fasta2len.py seq.fasta > seq.len
+    print the length of each entry in the input fasta file
+'''
+import sys
+def fasta2len(infile="seq.fasta"):
+    '''calculat the length of each entry in the input fasta file "infile"'''
+    len_txt=''
+    fp=open(infile,'rU')
+    txt=fp.read()
+    fp.close()
+    for block in txt.split('>'):
+        block=block.strip()
+        if not block:
+            continue
+        lines=block.splitlines()
+        header=lines[0].split()[0]
+        sequence=''.join(lines[1:])
+        len_txt+="%s\t%u\n"%(header,len(sequence))
+    return len_txt
+
+if __name__=="__main__":
+    if len(sys.argv)<2:
+        sys.stderr.write(docstring)
+        exit()
+
+    len_txt=fasta2len(infile=sys.argv[1])
+    if len(sys.argv)==2:
+        sys.stdout.write(len_txt)
+    else:
+        fp=open(sys.argv[2],'w')
+        fp.write(len_txt)
+        fp.close()
