5b2392e8964fc5d5b88ae0f1c6eee079f7f8d358
zcx <zcx@umich.edu>
2018-01-01 20:58:31 -0500

    pdb2sart print solvent accessibility

--- pdb2sarst.py
+++ pdb2sarst.py
@@ -38,6 +38,16 @@ options:
               protein is alpha(a) if >90% of non-coil residues are within helix
               protein is beta (b) if >90% of non-coil residues are within strand
               otherwise a protein is alpha-beta (ab)
+
+    -show_acc=0,1,2
+        whether to print relative solvent accessibility
+        0 - (default) do not print solvent accessibility
+        1 - horizontally print relative solvent accessibility. 
+            only first digit after decimal point
+        2 - vertically print relative solvent accessibility
+        3 - vertically print absolute solvent accessibility
+        4 - show total relative solvent accessibility in header
+        5 - show total absolute solvent accessibility in header
 '''
 import sys,os
 import subprocess
@@ -82,6 +92,49 @@ sarst_matrix=[
     "SSSSSQQQQQQQQQQZZZZZZRRRRRRYYYYYSSSS",
     "SSSSIIQQQQQQQQQZZZZZZZRRRYYYYYYYSSSS",
 ]
+
+MaxASA_dict={
+# data from Tien et al (2013). 
+# "Maximum allowed solvent accessibilites of residues in proteins". 
+# PLoS ONE. 8 (11): e80635. doi:10.1371/journal.pone.0080635. PMID 24278298.
+#
+# Tien (theor) ,Tien (emp), Miller 1987, Rose 1985
+    'A':[ 129.0, 121.0, 113.0, 118.1],
+    'R':[ 274.0, 265.0, 241.0, 256.0],
+    'N':[ 195.0, 187.0, 158.0, 165.5],
+    'D':[ 193.0, 187.0, 151.0, 158.7],
+    'C':[ 167.0, 148.0, 140.0, 146.1],
+    'E':[ 223.0, 214.0, 183.0, 186.2],
+    'Q':[ 225.0, 214.0, 189.0, 193.2],
+    'G':[ 104.0,  97.0,  85.0,  88.1],
+    'H':[ 224.0, 216.0, 194.0, 202.5],
+    'I':[ 197.0, 195.0, 182.0, 181.0],
+    'L':[ 201.0, 191.0, 180.0, 193.1],
+    'K':[ 236.0, 230.0, 211.0, 225.8],
+    'M':[ 224.0, 203.0, 204.0, 203.4],
+    'F':[ 240.0, 228.0, 218.0, 222.8],
+    'P':[ 159.0, 154.0, 143.0, 146.8],
+    'S':[ 155.0, 143.0, 122.0, 129.8],
+    'T':[ 172.0, 163.0, 146.0, 152.5],
+    'W':[ 285.0, 264.0, 259.0, 266.3],
+    'Y':[ 263.0, 255.0, 229.0, 236.8],
+    'V':[ 174.0, 165.0, 160.0, 164.5],
+
+# extended from above
+    'J':[ 199.0, 193.0, 181.0,187.05],# (L+I)/2
+    'U':[ 167.0, 148.0, 140.0, 146.1], # C
+    'Z':[ 224.0, 214.0, 186.0, 189.7], # (Q+E)/2
+    'B':[ 194.0, 187.0, 154.5, 162.1], # (D+N)/2
+    'O':[ 201.0, 191.0, 180.0, 193.1], # K
+    'X':[191.02,182.19,165.99,172.92], # average for all
+}
+
+#aa_scale_dict={
+    #'A': 8.25, 'R': 5.53, 'N': 4.06, 'D': 5.45, 'C': 1.37,
+    #'Q': 3.93, 'Q': 6.75, 'G': 7.07, 'H': 2.27, 'I': 5.96,
+    #'L': 9.66, 'K': 5.84, 'M': 2.42, 'F': 3.86, 'P': 4.70,
+    #'S': 6.56, 'T': 5.34, 'W': 1.08, 'Y': 2.92, 'V': 6.87,
+#}
 
 def detectDSSP(dssp_path="dssp"):
     '''auto detect the location of dssp executable'''
@@ -173,7 +226,7 @@ def runDSSP(infile="pdb.pdb",dssp_path="dssp",pulchra_path="pulchra"):
     return stdout
 
 def parseDSSP(dssp_txt='',show_seq=True,show_ss=8,show_sarst=True, 
-    fold_type_cutoff=0.9):
+    fold_type_cutoff=0.9, show_acc=0):
     '''parse cleaned dssp output. 
     output:
         dssp_dict: a dict whose key is chain ID and value
@@ -187,6 +240,7 @@ def parseDSSP(dssp_txt='',show_seq=True,show_ss=8,show_sarst=True,
     dssp_dict=dict()
     ss_type_dict=dict()
     ss_num_dict=dict()
+    acc_dict=dict()
     for line in dssp_txt.splitlines():
         if len(line)<115:
             continue
@@ -197,6 +251,10 @@ def parseDSSP(dssp_txt='',show_seq=True,show_ss=8,show_sarst=True,
         if not chainID in dssp_dict:
             dssp_dict[chainID]=['']*sum([show_seq,show_ss>0,show_sarst])
             ss_num_dict[chainID]=[0]*4
+            if show_acc in [4,5]:
+                acc_dict[chainID]=0
+            else:
+                acc_dict[chainID]=''
 
         if show_seq:
             dssp_dict[chainID][0]+=AA
@@ -223,6 +281,18 @@ def parseDSSP(dssp_txt='',show_seq=True,show_ss=8,show_sarst=True,
             else:
                 SARST_CODE='X'
             dssp_dict[chainID][-1]+=SARST_CODE
+
+        if show_acc:
+            ACC=float(line[35:38])
+            if show_acc in [1,2,4]:
+                ACC/=MaxASA_dict[AA][0]
+            if show_acc==1:
+                acc_dict[chainID]+=str(min([9,int(10*ACC)]))
+            elif show_acc in [2,3]:
+                acc_dict[chainID]+=str(ACC)+'\n'
+            elif show_acc in [4,5]:
+                acc_dict[chainID]+=ACC
+
     for chainID in dssp_dict:
         ss_type_dict[chainID]=0
         if show_ss:
@@ -245,13 +315,14 @@ def parseDSSP(dssp_txt='',show_seq=True,show_ss=8,show_sarst=True,
                 #ss_type_dict[chainID]+=1
             #if set('BE').intersection(dssp_dict[chainID][show_seq]):
                 #ss_type_dict[chainID]+=2
-    return dssp_dict,ss_type_dict,ss_num_dict
+    return dssp_dict,ss_type_dict,ss_num_dict,acc_dict
 
 if __name__=="__main__":
     show_seq=True
     show_ss=8
     show_sarst=True
     show_chain=True
+    show_acc=0
     dssp_path=''
     pulchra_path=''
     fold_type_cutoff=0.9
@@ -272,6 +343,8 @@ if __name__=="__main__":
             pulchra_path=arg[len("-pulchra_path="):]
         elif arg.startswith('-fold_type_cutoff='):
             fold_type_cutoff=float(arg[len("-fold_type_cutoff="):])
+        elif arg.startswith('-show_acc='):
+            show_acc=int(arg[len("-show_acc="):])
         elif arg.startswith('-'):
             sys.stderr.write("ERROR! Unknown argument %s\n"%arg)
             exit()
@@ -290,22 +363,29 @@ if __name__=="__main__":
 
     for infile in argv:
         dssp_txt=runDSSP(infile,dssp_path,pulchra_path)
-        dssp_dict,ss_type_dict,ss_num_dict=parseDSSP(dssp_txt,show_seq,
-            show_ss,show_sarst, fold_type_cutoff)
+        dssp_dict,ss_type_dict,ss_num_dict,acc_dict=parseDSSP(dssp_txt,
+            show_seq, show_ss,show_sarst, fold_type_cutoff, show_acc)
 
         PDBID=os.path.basename(infile).split('.')[0]
         txt=''
         for chainID in dssp_dict:
             header='>'+PDBID+(':'+chainID)*show_chain
             if show_ss==1:
-                txt+=header+'\t%d'%ss_type_dict[chainID]+'\n'+''.join(
-                    [line+'\n' for line in dssp_dict[chainID
-                    ][:show_seq]+dssp_dict[chainID][show_seq+1:]])
+                header+='\t%d'%ss_type_dict[chainID]
             elif show_ss==2:
-                txt+=header+'\t'+'\t'.join(map(str,ss_num_dict[chainID])
-                    )+'\n'+''.join([line+'\n' for line in dssp_dict[chainID
+                header+='\t'+'\t'.join(map(str,ss_num_dict[chainID]))
+
+            if show_acc in [4,5]:
+                header+="\t%.2f"%acc_dict[chainID]
+
+            if show_ss in [1,2]:
+                txt+=header+'\n'+''.join(
+                    [line+'\n' for line in dssp_dict[chainID
                     ][:show_seq]+dssp_dict[chainID][show_seq+1:]])
             else:
                 txt+=header+'\n'+''.join(
                     [line+'\n' for line in dssp_dict[chainID]])
+
+            if show_acc in [1,2,3]:
+                txt+=acc_dict[chainID]+'\n'*(show_acc==1)
         sys.stdout.write(txt)
